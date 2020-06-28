1b979f30903e62930dcb9f9a975fc54285b90111
safikw <safikwidiantoro@gmail.com>
2020-06-28 14:21:21 +0700

    first commit

---
+++ resources/views/dashboards/index.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.master')
+
+@section('content')
+<div class="container-fluid">
+
+          <!-- Page Heading -->
+          <div class="d-sm-flex align-items-center justify-content-between mb-4">
+            <h1 class="h3 mb-0 text-gray-800">dashboard</h1>
+          </div>
+
+          
+
+          <!-- Content Row -->
+
+          <div class="row">
+
+           
+          </div>
+
+          
+
+        </div>
+
+@stop
